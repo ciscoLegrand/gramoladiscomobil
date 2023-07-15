31fc8ced0ab62de3330a220909bdf709bd9eea31
ciscoLegrand <cisco.glez@gmail.com>
2023-07-15 13:49:32 +0200

    use notification component on views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,7 +25,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
+        format.html { redirect_to category_url(@category), success: { title: @category.name, body: "Category successfully created" } }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
+        format.html { redirect_to category_url(@category), success: { title: @category.name, body: "Category successfully updated" } }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,10 +49,11 @@ class CategoriesController < ApplicationController
 
   # DELETE /categories/1 or /categories/1.json
   def destroy
+    name = @category.name
     @category.destroy
 
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
+      format.html { redirect_to categories_url, alert: { title: 'Success', body: "Category #{name} deleted"} }
       format.json { head :no_content }
     end
   end
@@ -60,7 +61,7 @@ class CategoriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
-      @category = Category.find(params[:id])
+      @category = Category.friendly.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
