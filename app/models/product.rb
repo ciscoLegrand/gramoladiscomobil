fb801218aa6984cdc96e0a49ebe1d28487b25b8d
ciscoLegrand <cisco.glez@gmail.com>
2023-07-15 14:43:09 +0200

    add image variant on products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,10 @@ class Product < ApplicationRecord
   belongs_to :category
   has_rich_text :description
 
-  has_many_attached :images
+  has_many_attached :images do |attachable|
+    attachable.variant :mobile, resize_to_limit: [480, 800]
+    attachable.variant :tablet, resize_to_limit: [800, 1280]
+    attachable.variant :desktop, resize_to_limit: [1280, 1024]
+    attachable.variant :widescreen, resize_to_limit: [1920, 1080]
+  end
 end
