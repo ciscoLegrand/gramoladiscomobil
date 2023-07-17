b79e8d6e2ddeda73a8986661dfa56da4c4604f34
ciscoLegrand <cisco.glez@gmail.com>
2023-07-17 09:26:24 +0200

    update image preview controller and form views

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -1,38 +1,68 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input", "preview", "svg"];
+  static targets = ["input", "preview", "svg", "message"];
 
-  // connect() {
-  //   console.log("Entrando a connect");
-  //   this.previewImage();
-  // }
+  connect() {
+    console.log("ImagePreviewController connected");
+  }
 
   previewImage() {
-    console.log("Entrando a previewImage");
     let input = this.inputTarget;
     let preview = this.previewTarget;
-    let svg = this.svgTarget;
-    console.log(preview)
-    if (input.files && input.files[0]) {
-      console.log("Archivo seleccionado:", input.files[0]);
-      let reader = new FileReader();
-      if (svg) {
-        preview.classList.remove("hidden");
+    let message = this.messageTarget;
+    
+    if (input.files && input.files.length) {
+      console.log(`Archivos seleccionados: ${input.files.length}`);
+      
+      // check if there are svg or image elements and hide them
+      if (this.hasSvgTarget) {
+        let svg = this.svgTarget;
         svg.classList.add("hidden");
       }
-
-      reader.onload = (e) => {
-        console.log("Imagen cargada:", e.target.result);
-
-        preview.src = e.target.result;
-        console.log(e.target.result)
-      };
-
-      reader.readAsDataURL(input.files[0]);
+      
+      if (this.hasImageTarget) {
+        let image = this.imageTarget;
+        image.classList.add("hidden");
+      }
+      
+      // borra la vista previa anterior
+      while (preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+      }
+      
+      if (input.files.length <= 10) {
+        message.textContent = '';
+        for (let i = 0; i < input.files.length; i++) {
+          let reader = new FileReader();
+          
+          reader.onload = (e) => {
+            console.log(`Imagen ${i} cargada:`, e.target.result);
+            
+            let img = document.createElement('img');
+            img.src = e.target.result;
+            
+            // agrega la nueva imagen a la vista previa
+            preview.appendChild(img);
+          };
+          
+          reader.readAsDataURL(input.files[i]);
+        }
+      } else {
+        // ocultar la vista previa y mostrar un mensaje
+        preview.classList.add("hidden");
+        message.classList.remove("hidden");
+        message.classList.add("text-withe","w-full","px-4", "py-2", "text-center", "bg-green-500", "border-green-500","border", "border-2", "rounded-md");
+        message.textContent = `Se han seleccionado ${input.files.length} archivos`;
+      }
     } else {
-      console.log("No hay archivo seleccionado");
-      preview.src = "";
+      console.log("No hay archivos seleccionados");
+      // borra la vista previa anterior
+      while (preview.firstChild) {
+        preview.removeChild(preview.firstChild);
+      }
+      message.textContent = '';
     }
   }
+
 }
