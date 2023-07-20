3faf254db527bc8b14c754ff4a70bc0d8251a5f9
ciscoLegrand <cisco.glez@gmail.com>
2023-07-20 09:13:56 +0200

    fix contacts controller and mailer

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ContactsController < ApplicationController
   layout 'admin'
-  before_action :set_contact, only: %i[ show edit update destroy ]
+  before_action :set_contact, only: %i[ show destroy ]
 
   # GET /contacts or /contacts.json
   def index
@@ -9,27 +9,6 @@ class Admin::ContactsController < ApplicationController
 
   # GET /contacts/1 or /contacts/1.json
   def show
-  end
-
-  # GET /contacts/new
-  def new
-    @contact = Contact.new
-  end
-
-  # POST /contacts or /contacts.json
-  def create
-    @contact = Contact.new(contact_params)
-
-    respond_to do |format|
-      if @contact.save
-        ContactMailer.client_email(@contact).deliver_later
-        ContactMailer.admin_email(@contact).deliver_later
-        format.html { redirect_to root_path, success: { title: 'Solicitud enviada', body: "Recibiras una respuesta lo antes posible" } }
-      else
-        format.html { redirect_to root_path, error: { title: 'Error', body: "No se ha podido enviar tu consulta" } }
-        format.json { render json: @contact.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /contacts/1 or /contacts/1.json
@@ -46,12 +25,5 @@ class Admin::ContactsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_contact
       @contact = Contact.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def contact_params
-      params.require(:contact)
-            .permit(:name, :phone, :email, :title, :body,
-                    data: [:location, :date_event, :main_service, :extra_services])
     end
 end
