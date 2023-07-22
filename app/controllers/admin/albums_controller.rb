6412db7d0ed89ccd2656ab0d3f82e2e85b4d0961
ciscoLegrand <cisco.glez@gmail.com>
2023-07-22 21:31:50 +0200

    add turbostream responses on album actions

--- app/controllers/admin/albums_controller.rb
+++ app/controllers/admin/albums_controller.rb
@@ -12,6 +12,12 @@ class Admin::AlbumsController < ApplicationController
       
     @total_records = @albums.count
     @pagy, @albums = pagy(@albums, items: 5)
+
+    respond_to do |format|
+      format.html # GET
+      format.turbo_stream # POST
+      format.json { render json: @albums }
+    end
   end
 
   # GET /admin/albums/1 or /admin/albums/1.json
@@ -34,6 +40,7 @@ class Admin::AlbumsController < ApplicationController
     respond_to do |format|
       if album.save 
         format.html { redirect_to edit_admin_album_url(album), success: { title: "Success", body: "Album was successfully created." } }
+        format.turbo_stream { render turbo_stream: turbo_stream.prepend("albums", partial: "admin/albums/album", locals: { album: album }) }
         format.json { render :show, status: :created, location: album }
       else
         format.html { redirect_to new_admin_album_url, alert: { title: 'no se ha creado ', body: album.errors.full_messages } }
@@ -47,6 +54,7 @@ class Admin::AlbumsController < ApplicationController
     respond_to do |format|
       if @album.update(album_params)
         format.html { redirect_to admin_album_url(@album), success: { title: "Success", body: "Album was successfully updated." } }
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@album, partial: "admin/albums/album", locals: { album: @album }) }
         format.json { render :show, status: :ok, location: @album }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -58,12 +66,11 @@ class Admin::AlbumsController < ApplicationController
   # DELETE /admin/albums/1 or /admin/albums/1.json
   def destroy
     name = @album.title
-    images = @album.images.size
-  
     PurgeImagesJob.perform_later(@album)
   
     respond_to do |format|
       format.html { redirect_to admin_albums_url, success: {title: "album #{name} se va a borrar.", body: "Recibirás un email con la informacón de eliminación."} }
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@album) }
       format.json { head :no_content }
     end
   end
@@ -75,6 +82,7 @@ class Admin::AlbumsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to admin_album_url(@album), success: { title: "Success", body: "Album was successfully published." } }
       format.turbo_stream { render turbo_stream: turbo_stream.replace(@album, partial: "admin/albums/album", locals: { album: @album }) }
+      format.json { render :show, status: :ok, location: @album }
     end
   end
 
